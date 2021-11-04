81a74b3453ab6b576f7742ad990b8480fc54934a
céu <60863899+GiovanaKr@users.noreply.github.com>
2021-11-03 21:17:49 -0300

    Update Compras.py
    atualização

--- SenaiPython/Compras.py
+++ SenaiPython/Compras.py
@@ -3,10 +3,8 @@ import os
 clear = lambda: os.system('cls')
 lista = ObjetoPedido.listaP
 
-pp = ObjetoPedido.Pedido("papel","100")
-lista.append(pp)
-gg = ObjetoPedido.Pedido("caneta","5")
-lista.append(gg)
+#gg = ObjetoPedido.Pedido('papel','100')
+#lista.append(gg)
 
 class Compras:
     def Main():
@@ -17,6 +15,7 @@ class Compras:
         r = input(": ")
         if int(r) == 1:
             Compras.Lista()
+            Compras.Main()
         elif int(r) == 5:
             return
         else:
@@ -24,8 +23,10 @@ class Compras:
 
     def Lista():
         clear()
+        h = 0
+        print("pressione '0' para sair")
+        print("")
         for x in range(len(lista)): #para cada item na lista
-           
             if int(lista[x].aprovGen) == 1 :
                 print(str(x)+" "+"Item: "+lista[x].qtd+" "+lista[x].nome)
                 if int(lista[x].aprovCom) == 2:
@@ -35,21 +36,30 @@ class Compras:
                 else:
                     print("Negado")
                 print("")
-
-        h = 0
-        for x in range(len(lista)):
-            if int(lista[x].aprovGen) == 1 :
-                y = input("Modificar item nº:")
-                if x == int(y):
-                    print(lista[x].qtd + " " +lista[x].nome)
-                    r = input("Aprovar(1)   Reprovar(0)")
-                    lista[x].aprovCom = r
-                    h=h+1
-                if  int(r) == 0:
-                    lista[x].justificava=input('Motivo:')    
+                h=h+1
 
         if h == 0 :
-            print("Aguardando requisições")  
-
-        x = input("")
-        Compras.Main()
+            print("Aguardando requisições") 
+            f = input('')
+            return
+        else:
+            y = input("Modificar item nº:")
+            d = int(y)-1 #numero certo
+            if int(y) == 0:
+                return
+            for x in range(len(lista)):
+                if int(lista[x].aprovGen) == 1 :
+                    if x == d:
+                        print(lista[x].qtd + " " +lista[x].nome)
+                        r = input("Aprovar(1)   Reprovar(0)")
+                        if r == 's':
+                            Compras.Main() 
+                        if  int(r) == 0:
+                            lista[x].justificava=input('Motivo:')                      
+                        if int(r) == 0 or int(r) == 1:
+                            lista[x].aprovCom = r 
+                        else:
+                            print("input incorreto")
+                            x = input("")
+                            Compras.Lista()                    
+        Compras.Lista()
